14dfda78bf42ea5d7513003bf97faf3dabbd743f
Isaiah Ojo <51814402+LafyOjo@users.noreply.github.com>
2023-04-24 20:00:24 +0100

    Initial commit

---
+++ ITC-Assignment-16/ITC_Assignment_16App.swift
@@ -0,0 +1,17 @@
+//
+//  ITC_Assignment_16App.swift
+//  ITC-Assignment-16
+//
+//  Created by Isaiah Ojo on 24/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ITC_Assignment_16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
